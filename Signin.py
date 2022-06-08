ec6656769a30cefe1c4083e181f0c3340381d78a
Nguyen Son <son.n@ThanhNQ-MacMN.local>
2022-06-08 16:20:49 +0700

    code xong

--- Signin.py
+++ Signin.py
@@ -3,28 +3,26 @@ from tkinter import *
 from tkinter.ttk import *
 from tkinter import messagebox
 import json
-from numpy import size
 import tkinter as tk
 import re
-
+import hashlib
 regex_email = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
-regex_name = "^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)"
 
 def valid_email(input):
     if(re.search(regex_email,input) and input.isalpha):
-        btnSignup.config(state='active')  
+        btnSignin.config(state='active')  
         return True        
     else:
-        btnSignup.config(state='disabled')  
+        btnSignin.config(state='disabled')  
         return False 
 
-def valid_name(input):
-    if(re.search(regex_name,input) and input.isalpha):
-        btnSignup.config(state='active')  
-        return True        
-    else:
-        btnSignup.config(state='disabled')  
-        return False 
+def check_password(hashed_password, user_password):
+    password, salt = hashed_password.split(':')
+    return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+
+def checkAccount():
+   data = open('/Users/anhquantran/Documents/GitHub/App/user.txt').read() 
+
 
 #data of user
 data = {}
@@ -32,10 +30,9 @@ data['user'] = []
 
 # Tạo giao diện
 window = tk.Tk()
-window.geometry("500x700")
+window.geometry("500x300")
 window.title("Mã hóa")
 mailValid = window.register(valid_email)
-nameValid = window.register(valid_name)
 
 email = tk.StringVar()
 name = tk.StringVar()
@@ -44,68 +41,22 @@ phone = tk.StringVar()
 address = tk.StringVar()
 passphase = tk.StringVar()
 
-#save data after click register button
-def register_click():
-    data['user'].append({
-        'email': email.get(),
-        'name': name.get(),
-        'dob': dob.get(),
-        'phone': phone.get(),
-        'address': address.get(),
-        'passphase': passphase.get()
-    })
-    with open('/Users/anhquantran/Documents/GitHub/App/user.txt', 'a') as outfile:
-        json.dump(data, outfile)
-
-def check_empty_email() :
-     if eEmail.get():
-         pass     #your function where you want to jump
-     else:
-        print('Input email required')
-
-def check_empty_name() :
-     if eName.get():
-         pass     #your function where you want to jump
-     else:
-        print('Input name required')
-
-def combine_funcs(*funcs):
-    def combined_func(*args, **kwargs):
-        for f in funcs:
-            f(*args, **kwargs)
-    return combined_func
 
 # Body
 lbSignup = Label(window, text="SIGN IN", font=("arial", 25))
 lbSignup.place(x=200, y=10)
 lbEmail = Label(window, text='Email',font=('arial',15))
 lbEmail.place(x = 50,y = 100)
-lbName = Label(window, text='Name',font=('arial',15))
-lbName.place(x = 50,y = 150)
-lbDOB = Label(window, text='Date of birth',font=('arial',15))
-lbDOB.place(x = 50,y = 200)
-lbPhoneNumber = Label(window, text='Phone number',font=('arial',15))
-lbPhoneNumber.place(x = 50,y = 250)
-lbAddress = Label(window, text='Address',font=('arial',15))
-lbAddress.place(x = 50,y = 300)
 lbPassphase = Label(window, text='Passphase',font=('arial',15))
-lbPassphase.place(x = 50,y = 350)
+lbPassphase.place(x = 50,y = 150)
 
 eEmail = Entry(window,font = ('Arial',15),textvariable=email,validate='focusout',validatecommand=(mailValid,'%P'))
 eEmail.place(x= 200, y= 100)
-eName = Entry(window,font = ('Arial',15),textvariable=name,validate='focusout',validatecommand=(nameValid,'%P'))
-eName.place(x= 200, y= 150)
-eDOB = Entry(window,font = ('Arial',15),textvariable=dob)
-eDOB.place(x= 200, y= 200)
-ePhoneNumber = Entry(window,font = ('Arial',15),textvariable=phone)
-ePhoneNumber.place(x= 200, y= 250)
-eAddress = Entry(window,font = ('Arial',15),textvariable=address)
-eAddress.place(x= 200, y= 300)
 ePassphase = Entry(window,font = ('Arial',15),textvariable=passphase,show='*')
-ePassphase.place(x= 200, y= 350)
+ePassphase.place(x= 200, y= 150)
 
-btnSignup = Button(window, text="REGISTER",state=DISABLED,command=combine_funcs(register_click, check_empty_email, check_empty_name))
-btnSignup.place(x=210, y=500)
+btnSignin = Button(window, text="LOGIN",state=DISABLED,command=checkAccount)
+btnSignin.place(x=210, y=500)
 
 # Hiển thị
 window.mainloop()
---
+++ Signup.py
@@ -0,0 +1,115 @@
+from cProfile import label
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import messagebox
+import json
+import tkinter as tk
+import re
+import hashlib
+import uuid
+
+regex_email = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+regex_name = "^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)"
+
+def valid_email(input):
+    if(re.search(regex_email,input) and input.isalpha):
+        btnSignup.config(state='active')  
+        return True        
+    else:
+        btnSignup.config(state='disabled')  
+        return False 
+
+def valid_name(input):
+    if(re.search(regex_name,input) and input.isalpha):
+        btnSignup.config(state='active')  
+        return True        
+    else:
+        btnSignup.config(state='disabled')  
+        return False 
+
+#data of user
+data = {}
+data['user'] = []
+
+# Tạo giao diện
+window = tk.Tk()
+window.geometry("500x700")
+window.title("Mã hóa")
+mailValid = window.register(valid_email)
+nameValid = window.register(valid_name)
+
+email = tk.StringVar()
+name = tk.StringVar()
+dob = tk.StringVar()
+phone = tk.StringVar()
+address = tk.StringVar()
+passphase = tk.StringVar()
+
+#save data after click register button
+def register_click():
+    salt = uuid.uuid4().hex
+    hash_object = hashlib.sha256(salt.encode() + str(passphase.get()).encode('utf-8'))
+
+    data['user'].append({
+        'email': email.get(),
+        'name': name.get(),
+        'dob': dob.get(),
+        'phone': phone.get(),
+        'address': address.get(),
+        'passphase': hash_object.hexdigest()+':'+salt
+    })
+    with open('/Users/son.n/Desktop/Ngay 8_6/App/user.txt', 'a') as outfile:
+        json.dump(data, outfile)
+
+def check_empty_email() :
+     if eEmail.get():
+         pass     #your function where you want to jump
+     else:
+        print('Input email required')
+
+def check_empty_name() :
+     if eName.get():
+         pass     #your function where you want to jump
+     else:
+        print('Input name required')
+
+def combine_funcs(*funcs):
+    def combined_func(*args, **kwargs):
+        for f in funcs:
+            f(*args, **kwargs)
+    return combined_func
+
+# Body
+lbSignup = Label(window, text="SIGN UP", font=("arial", 25))
+lbSignup.place(x=200, y=10)
+lbEmail = Label(window, text='Email',font=('arial',15))
+lbEmail.place(x = 50,y = 100)
+lbName = Label(window, text='Name',font=('arial',15))
+lbName.place(x = 50,y = 150)
+lbDOB = Label(window, text='Date of birth',font=('arial',15))
+lbDOB.place(x = 50,y = 200)
+lbPhoneNumber = Label(window, text='Phone number',font=('arial',15))
+lbPhoneNumber.place(x = 50,y = 250)
+lbAddress = Label(window, text='Address',font=('arial',15))
+lbAddress.place(x = 50,y = 300)
+lbPassphase = Label(window, text='Passphase',font=('arial',15))
+lbPassphase.place(x = 50,y = 350)
+
+eEmail = Entry(window,font = ('Arial',15),textvariable=email,validate='focusout',validatecommand=(mailValid,'%P'))
+eEmail.place(x= 200, y= 100)
+eName = Entry(window,font = ('Arial',15),textvariable=name,validate='focusout',validatecommand=(nameValid,'%P'))
+eName.place(x= 200, y= 150)
+eDOB = Entry(window,font = ('Arial',15),textvariable=dob)
+eDOB.place(x= 200, y= 200)
+ePhoneNumber = Entry(window,font = ('Arial',15),textvariable=phone)
+ePhoneNumber.place(x= 200, y= 250)
+eAddress = Entry(window,font = ('Arial',15),textvariable=address)
+eAddress.place(x= 200, y= 300)
+ePassphase = Entry(window,font = ('Arial',15),textvariable=passphase,show='*')
+ePassphase.place(x= 200, y= 350)
+
+btnSignup = Button(window, text="REGISTER",state=DISABLED,command=combine_funcs(register_click, check_empty_email, check_empty_name))
+btnSignup.place(x=210, y=500)
+
+# Hiển thị
+window.mainloop()
