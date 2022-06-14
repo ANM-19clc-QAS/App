44dca90113e0f9a819171dbebd517a7bc500b8d7
Trần Anh Quân <trananhquanttbpvn@gmail.com>
2022-06-14 22:55:48 +0700

    Update Edit info

--- EditInfo.py
+++ EditInfo.py
@@ -19,3 +19,14 @@ import datetime as dt
 # with open(path+'user.txt') as fin:
 #     data = json.load(fin)
 print('hehe: '+str(email.get()))
+
+winei = tk.Tk()
+winei.geometry("500x600")
+winei.title("Mã hóa")
+mailValid = winei.register(valid_email)
+nameValid = winei.register(valid_name)
+dobValid = winei.register(valid_dob)
+phoneValid = winei.register(valid_phone)
+passValid = winei.register(valid_pass)
+
+
